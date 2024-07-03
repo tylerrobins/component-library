b8b48dcc45547dc52d92f672da31006a59c7e3ca
TylerKingsley <tyler@kingsleyrobins.com>
2024-07-03 20:15:03 +1000

    Storybook-static created.

---
+++ storybook-static/assets/Accordion.stories-Bli6XXah.js
@@ -0,0 +1,50 @@
+import{j as e}from"./jsx-runtime-CGpYA4L6.js";import{R as l,r as D}from"./index-CleY8y_P.js";import{c as me,P as q}from"./index-BUwirNCq.js";import{c as ue,u as Ae}from"./index-B57e_qeq.js";import{u as fe}from"./index-1PiYJVay.js";import{u as F,c as ge}from"./index-CRjgN0eO.js";import{c as J,R as xe,T as he,b as ve}from"./index-Dy3DlD1s.js";import{u as Ie}from"./index-DF5SYaCP.js";import{c as O}from"./utils-C-5SdYIA.js";import{C as Ce}from"./chevron-down-DmK9nTZU.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C2jgcCTd.js";import"./index-FvTp1rED.js";import"./createLucideIcon-kBjshEDv.js";var m="Accordion",be=["Home","End","ArrowDown","ArrowUp","ArrowLeft","ArrowRight"],[M,ye,je]=ue(m),[j,Fe]=me(m,[je,J]),V=J(),Q=l.forwardRef((r,c)=>{const{type:o,...t}=r,i=t,n=t;return e.jsx(M.Provider,{scope:r.__scopeAccordion,children:o==="multiple"?e.jsx(Ne,{...n,ref:c}):e.jsx(Re,{...i,ref:c})})});Q.displayName=m;var[X,_e]=j(m),[Z,we]=j(m,{collapsible:!1}),Re=l.forwardRef((r,c)=>{const{value:o,defaultValue:t,onValueChange:i=()=>{},collapsible:n=!1,...s}=r,[a,d]=F({prop:o,defaultProp:t,onChange:i});return e.jsx(X,{scope:r.__scopeAccordion,value:a?[a]:[],onItemOpen:d,onItemClose:l.useCallback(()=>n&&d(""),[n,d]),children:e.jsx(Z,{scope:r.__scopeAccordion,collapsible:n,children:e.jsx(ee,{...s,ref:c})})})}),Ne=l.forwardRef((r,c)=>{const{value:o,defaultValue:t,onValueChange:i=()=>{},...n}=r,[s=[],a]=F({prop:o,defaultProp:t,onChange:i}),d=l.useCallback(f=>a((u=[])=>[...u,f]),[a]),A=l.useCallback(f=>a((u=[])=>u.filter(w=>w!==f)),[a]);return e.jsx(X,{scope:r.__scopeAccordion,value:s,onItemOpen:d,onItemClose:A,children:e.jsx(Z,{scope:r.__scopeAccordion,collapsible:!0,children:e.jsx(ee,{...n,ref:c})})})}),[Te,_]=j(m),ee=l.forwardRef((r,c)=>{const{__scopeAccordion:o,disabled:t,dir:i,orientation:n="vertical",...s}=r,a=l.useRef(null),d=fe(a,c),A=ye(o),u=Ae(i)==="ltr",w=ge(r.onKeyDown,v=>{var W;if(!be.includes(v.key))return;const de=v.target,R=A().filter(E=>{var K;return!((K=E.ref.current)!=null&&K.disabled)}),I=R.findIndex(E=>E.ref.current===de),H=R.length;if(I===-1)return;v.preventDefault();let p=I;const N=0,T=H-1,S=()=>{p=I+1,p>T&&(p=N)},P=()=>{p=I-1,p<N&&(p=T)};switch(v.key){case"Home":p=N;break;case"End":p=T;break;case"ArrowRight":n==="horizontal"&&(u?S():P());break;case"ArrowDown":n==="vertical"&&S();break;case"ArrowLeft":n==="horizontal"&&(u?P():S());break;case"ArrowUp":n==="vertical"&&P();break}const pe=p%H;(W=R[pe].ref.current)==null||W.focus()});return e.jsx(Te,{scope:o,disabled:t,direction:i,orientation:n,children:e.jsx(M.Slot,{scope:o,children:e.jsx(q.div,{...s,"data-orientation":n,ref:d,onKeyDown:t?void 0:w})})})}),y="AccordionItem",[Se,Y]=j(y),oe=l.forwardRef((r,c)=>{const{__scopeAccordion:o,value:t,...i}=r,n=_(y,o),s=_e(y,o),a=V(o),d=Ie(),A=t&&s.value.includes(t)||!1,f=n.disabled||r.disabled;return e.jsx(Se,{scope:o,open:A,disabled:f,triggerId:d,children:e.jsx(xe,{"data-orientation":n.orientation,"data-state":se(A),...a,...i,ref:c,disabled:f,open:A,onOpenChange:u=>{u?s.onItemOpen(t):s.onItemClose(t)}})})});oe.displayName=y;var re="AccordionHeader",ne=l.forwardRef((r,c)=>{const{__scopeAccordion:o,...t}=r,i=_(m,o),n=Y(re,o);return e.jsx(q.h3,{"data-orientation":i.orientation,"data-state":se(n.open),"data-disabled":n.disabled?"":void 0,...t,ref:c})});ne.displayName=re;var k="AccordionTrigger",te=l.forwardRef((r,c)=>{const{__scopeAccordion:o,...t}=r,i=_(m,o),n=Y(k,o),s=we(k,o),a=V(o);return e.jsx(M.ItemSlot,{scope:o,children:e.jsx(he,{"aria-disabled":n.open&&!s.collapsible||void 0,"data-orientation":i.orientation,id:n.triggerId,...a,...t,ref:c})})});te.displayName=k;var ce="AccordionContent",ie=l.forwardRef((r,c)=>{const{__scopeAccordion:o,...t}=r,i=_(m,o),n=Y(ce,o),s=V(o);return e.jsx(ve,{role:"region","aria-labelledby":n.triggerId,"data-orientation":i.orientation,...s,...t,ref:c,style:{"--radix-accordion-content-height":"var(--radix-collapsible-content-height)","--radix-accordion-content-width":"var(--radix-collapsible-content-width)",...r.style}})});ie.displayName=ce;function se(r){return r?"open":"closed"}var Pe=Q,Ee=oe,ke=ne,ae=te,le=ie;const De=Pe,g=D.forwardRef(({className:r,...c},o)=>e.jsx(Ee,{ref:o,className:O("border-b",r),...c}));g.displayName="AccordionItem";const x=D.forwardRef(({className:r,children:c,...o},t)=>e.jsx(ke,{className:"flex",children:e.jsxs(ae,{ref:t,className:O("flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",r),...o,children:[c,e.jsx(Ce,{className:"h-4 w-4 shrink-0 transition-transform duration-200"})]})}));x.displayName=ae.displayName;const h=D.forwardRef(({className:r,children:c,...o},t)=>e.jsx(le,{ref:t,className:"overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",...o,children:e.jsx("div",{className:O("pb-4 pt-0",r),children:c})}));h.displayName=le.displayName;try{g.displayName="AccordionItem",g.__docgenInfo={description:"",displayName:"AccordionItem",props:{asChild:{defaultValue:null,description:"",name:"asChild",required:!1,type:{name:"boolean"}}}}}catch{}const Je={title:"BlackBox/Accordion",component:De,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{type:{control:"select",options:["single","multiple"],description:"The type of accordion"},collapsible:{control:"boolean",description:"Whether the accordion items are collapsible"},children:{control:void 0,description:"Accordion content"}}},C={args:{type:"single",collapsible:!1,children:e.jsxs(g,{value:"item-1",children:[e.jsx(x,{children:"Is it accessible?"}),e.jsx(h,{children:"Yes. It adheres to the WAI-ARIA design pattern."})]})},argTypes:{type:{control:!1,table:{disable:!0}}}},b={args:{type:"multiple",children:e.jsxs(e.Fragment,{children:[e.jsxs(g,{value:"item-1",children:[e.jsx(x,{children:"Is it accessible?"}),e.jsx(h,{children:"Yes. It adheres to the WAI-ARIA design pattern."})]}),e.jsxs(g,{value:"item-2",children:[e.jsx(x,{children:"Is it styled?"}),e.jsx(h,{children:"Yes. It is styled using Tailwind CSS."})]}),e.jsxs(g,{value:"item-3",children:[e.jsx(x,{children:"Is it responsive?"}),e.jsx(h,{children:"Yes. It is fully responsive."})]})]})},argTypes:{type:{control:!1,table:{disable:!0}}}};var $,L,z;C.parameters={...C.parameters,docs:{...($=C.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    type: "single",
+    collapsible: false,
+    children: <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+  },
+  argTypes: {
+    type: {
+      control: false,
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(z=(L=C.parameters)==null?void 0:L.docs)==null?void 0:z.source}}};var B,G,U;b.parameters={...b.parameters,docs:{...(B=b.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    type: "multiple",
+    children: <>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It is styled using Tailwind CSS.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it responsive?</AccordionTrigger>
+          <AccordionContent>Yes. It is fully responsive.</AccordionContent>
+        </AccordionItem>
+      </>
+  },
+  argTypes: {
+    type: {
+      control: false,
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(U=(G=b.parameters)==null?void 0:G.docs)==null?void 0:U.source}}};const Qe=["Single","Multiple"];export{b as Multiple,C as Single,Qe as __namedExportsOrder,Je as default};
---
+++ storybook-static/assets/Alert.stories-C3zsw0X6.js
@@ -0,0 +1,73 @@
+import{j as e}from"./jsx-runtime-CGpYA4L6.js";import{r as d}from"./index-CleY8y_P.js";import{c as N}from"./index-Bb4qSo10.js";import{c as p}from"./utils-C-5SdYIA.js";import{c as A}from"./createLucideIcon-kBjshEDv.js";import"./_commonjsHelpers-Cpj98o6Y.js";/**
+ * @license lucide-react v0.400.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const D=A("CircleAlert",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]]);/**
+ * @license lucide-react v0.400.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const T=A("Terminal",[["polyline",{points:"4 17 10 11 4 5",key:"akl6gq"}],["line",{x1:"12",x2:"20",y1:"19",y2:"19",key:"q2wloq"}]]),k=N("relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",{variants:{variant:{default:"bg-background text-foreground",destructive:"border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive"}},defaultVariants:{variant:"default"}}),i=d.forwardRef(({className:r,variant:t,...a},j)=>e.jsx("div",{ref:j,role:"alert",className:p(k({variant:t}),r),...a}));i.displayName="Alert";const o=d.forwardRef(({className:r,...t},a)=>e.jsx("h5",{ref:a,className:p("mb-1 font-medium leading-none tracking-tight",r),...t}));o.displayName="AlertTitle";const c=d.forwardRef(({className:r,...t},a)=>e.jsx("div",{ref:a,className:p("text-sm [&_p]:leading-relaxed",r),...t}));c.displayName="AlertDescription";try{i.displayName="Alert",i.__docgenInfo={description:"",displayName:"Alert",props:{variant:{defaultValue:null,description:"",name:"variant",required:!1,type:{name:'"default" | "destructive" | null'}}}}}catch{}const Y={title:"BlackBox/Alert",component:i,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{control:"select",options:["default","destructive"],description:"The variant of the alert"},children:{control:void 0,description:"Alert content"}}},n={args:{variant:"default",children:e.jsxs(e.Fragment,{children:[e.jsx(T,{className:"h-4 w-4"}),e.jsx(o,{children:"Alert!"}),e.jsx(c,{children:"This is the control alert..."})]})},argTypes:{children:{table:{disable:!0}}}},s={args:{variant:"default",children:e.jsxs(e.Fragment,{children:[e.jsx(T,{className:"h-4 w-4"}),e.jsx(o,{children:"Heads up!"}),e.jsx(c,{children:"You can add components to your app using the cli."})]})},argTypes:{variant:{table:{disable:!0}},children:{table:{disable:!0}}}},l={args:{variant:"destructive",children:e.jsxs(e.Fragment,{children:[e.jsx(D,{className:"h-4 w-4"}),e.jsx(o,{children:"Error"}),e.jsx(c,{children:"Your session has expired. Please log in again."})]})},argTypes:{variant:{table:{disable:!0}},children:{table:{disable:!0}}}};var u,m,g;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: "default",
+    children: <>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Alert!</AlertTitle>
+        <AlertDescription>This is the control alert...</AlertDescription>
+      </>
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(g=(m=n.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var h,v,x;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    variant: "default",
+    children: <>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </>
+  },
+  argTypes: {
+    variant: {
+      table: {
+        disable: true
+      }
+    },
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(x=(v=s.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var f,y,b;l.parameters={...l.parameters,docs:{...(f=l.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    variant: "destructive",
+    children: <>
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Your session has expired. Please log in again.
+        </AlertDescription>
+      </>
+  },
+  argTypes: {
+    variant: {
+      table: {
+        disable: true
+      }
+    },
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}`,...(b=(y=l.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};const F=["Control","Default","Destructive"];export{n as Control,s as Default,l as Destructive,F as __namedExportsOrder,Y as default};
