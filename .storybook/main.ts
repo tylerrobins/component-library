41f82ba55baa991a3690359284a51b3847d67b26
TylerKingsley <tyler@kingsleyrobins.com>
2024-07-04 19:18:49 +1000

    Hover Card story added

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,7 +16,7 @@ const config: StorybookConfig = {
     options: {},
   },
   typescript: {
-    reactDocgen: "react-docgen-typescript"
-  }
+    reactDocgen: "react-docgen-typescript",
+  },
 };
 export default config;
