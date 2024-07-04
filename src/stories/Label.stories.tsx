508c22302007dfd04513ba273b0d4d51160e3d78
TylerKingsley <tyler@kingsleyrobins.com>
2024-07-04 21:01:36 +1000

    Input-OTP and Menubar stories added

---
+++ src/stories/Label.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+
+const meta = {
+  title: "BlackBox/Label",
+  component: Label,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    htmlFor: {
+      control: "text",
+      description: "",
+    },
+    children: { control: "text", description: "Label text" },
+    className: { control: "text" },
+  },
+} satisfies Meta<typeof Label>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    htmlFor: "terms",
+    children: "Accept terms and conditions",
+  },
+  decorators: [
+    (Story) => (
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <Story />
+      </div>
+    ),
+  ],
+};
