9c7464feebcb5c00c2c23e521d211093930b70f3
TylerKingsley <tyler@kingsleyrobins.com>
2024-07-04 20:11:09 +1000

    Input added

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,192 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { withToastProvider } from "@/stories/helpers/withToastProvider";
+
+const meta = {
+  title: "BlackBox/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    className: {
+      control: "text",
+      description: "Classes to be applied",
+    },
+    type: {
+      control: "select",
+      options: [
+        "button",
+        "checkbox",
+        "color",
+        "date",
+        "datetime-local",
+        "email",
+        "file",
+        "hidden",
+        "image",
+        "month",
+        "number",
+        "password",
+        "radio",
+        "range",
+        "reset",
+        "search",
+        "submit",
+        "tel",
+        "text",
+        "time",
+        "url",
+        "week",
+      ],
+      description: "",
+    },
+    placeholder: {
+      control: "text",
+      description: "Placeholder for the input field",
+    },
+    id: {
+      control: "text",
+      description: "Input field id",
+    },
+    disabled: {
+      control: "boolean",
+      description: "Disables the input field",
+    },
+  },
+} satisfies Meta<typeof Input>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    type: "email",
+    placeholder: "Email",
+  },
+};
+
+export const File: Story = {
+  args: {
+    type: "file",
+    id: "picture",
+  },
+  decorators: [
+    (Story) => (
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="picture">Picture</Label>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export const Disabled: Story = {
+  args: {
+    type: "email",
+    placeholder: "Email",
+    disabled: true,
+  },
+};
+
+export const WithLabel: Story = {
+  args: {
+    type: "email",
+    id: "email",
+    placeholder: "Email",
+  },
+  decorators: [
+    (Story) => (
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export const WithButton: Story = {
+  args: {
+    type: "email",
+    placeholder: "Email",
+  },
+  decorators: [
+    (Story) => (
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Story />
+        <Button type="submit">Subscribe</Button>
+      </div>
+    ),
+  ],
+};
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export const InForm: Story = {
+  render: () => {
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+      defaultValues: {
+        username: "",
+      },
+    });
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+      toast({
+        title: "You submitted the following values:",
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      });
+    }
+
+    return (
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-2/3 space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    );
+  },
+  decorators: [withToastProvider],
+};
