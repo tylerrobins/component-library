69ce7106cc28f4dad3ea48c517874a9eff79f72f
TylerKingsley <tyler@kingsleyrobins.com>
2024-07-05 15:53:09 +1000

    Tooltip added

--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -1,13 +1,36 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Tooltip } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
 
 const meta = {
   title: "BlackBox/Tooltip",
-  component: Tooltip,
+  component: TooltipProvider,
   parameters: { layout: "centered" },
-} satisfies Meta<typeof Tooltip>;
+  tags: ["autodocs"],
+  argTypes: { children: { control: undefined } },
+} satisfies Meta<typeof TooltipProvider>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline">Hover</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </>
+    ),
+  },
+};
