30c4967b43de009df2edb2ce9f4924a3b64f965c
TylerKingsley <tyler@kingsleyrobins.com>
2024-07-05 14:50:04 +1000

    Switch & Sonner added

--- src/stories/Sonner.stories.tsx
+++ src/stories/Sonner.stories.tsx
@@ -1,13 +1,39 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
 
 const meta = {
   title: "BlackBox/Sonner",
-  component: Toaster,
   parameters: { layout: "centered" },
-} satisfies Meta<typeof Toaster>;
+  decorators: [
+    (Story) => (
+      <>
+        <Story />
+        <Toaster />
+      </>
+    ),
+  ],
+} satisfies Meta;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: () => (
+    <Button
+      variant="outline"
+      onClick={() =>
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        })
+      }
+    >
+      Show Toast
+    </Button>
+  ),
+};
